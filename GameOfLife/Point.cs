f318390c5d011bd5690ce28f3a1a5dfb6760fcc7
Shelgas <akozlov1997@gmail.com>
2022-03-05 14:01:19 +0300

    Added struct Point

---
+++ GameOfLife/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    internal struct Point
+    {
+        public readonly int X;
+        public readonly int Y;
+        public const char Symbol = ' ';
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
